72ac95cbf03dc29eb14894fc9d62ba04c727bfb0
tbapake <buatbelajarapake@gmail.com>
2019-01-08 14:20:47 +0700

    perbaikan glb

--- src/belajar/glb.go
+++ src/belajar/glb.go
@@ -3,15 +3,15 @@ package main
 
 import "fmt"
 
+var	s, v, t float64
+
 func main() {
-	var (
-		s, v, t float64
-	)
+	
 
 	fmt.Println("Masukan nilai jarak (dalam satuan Meter) :")
-	fmt.Scanf("%f", &s)
+	fmt.Scanln(&s)
 	fmt.Println("Masukan nilai waktu tempuh (dalam satuan detik) :")
-	fmt.Scanf("%f", &t)
+	fmt.Scanln(&t)
 	v = s / t
 	fmt.Println("Hasilnya adalah ",v, "m/s")
 }
